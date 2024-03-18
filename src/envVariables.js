2e67fc4edade8bbeeb339c4a51fb122c49483171
Rishi Raghav <rishiraghav1@gmail.com>
2024-03-18 05:59:55 -0400

    test

--- src/envVariables.js
+++ src/envVariables.js
@@ -1,9 +1,8 @@
 exports.handler = async function (event, context) {
-  const <var1> = process.env.OMDP_API_KEY;
-  const <var2> = process.env.OPENAI_API_KEY
+  const value = process.env.OPENAI_API_KEY;
 
   return {
     statusCode: 200,
-    body: JSON.stringify({ OPENAI_API_KEY: OPENAI_API_KEY, OMDP_API_KEY: OMDP_API_KEY}),
-  };
+    body: JSON.stringify({ message: `Value of MY_IMPORTANT_VARIABLE is ${value}.` }),
+  };  
 };
