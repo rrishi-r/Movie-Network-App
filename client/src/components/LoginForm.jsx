1159997716eda27cd55cfd16204e7cb963e3e0c0
Rishi Raghav <rishiraghav1@gmail.com>
2024-09-05 11:43:13 -0400

    add import

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
+import { toFormData } from 'axios';
 
 const LoginForm = () => {
     const navigate = useNavigate();
